ef5f6f50a986e7478d0d6282c452e7d85829db30
acrazybirdy <silas.y.vogel@web.de>
2023-01-23 21:46:09 +0100

    writer(bracket mistakes)

--- raw/firstboot.asm
+++ raw/firstboot.asm
@@ -1,4 +1,20 @@
-jmp $
+mov bx, buffer
+mov ah, 0x0e
+jmp write
+
+buffer:
+    dw "Hallo Welt", 0
+
+write:
+    mov al, [bx]
+    int 0x10
+    inc bx
+    cmp bx, 0
+    je end
+    jmp write
+
+end:
+    jmp $
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
